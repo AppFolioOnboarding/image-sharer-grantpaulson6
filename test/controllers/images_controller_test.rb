4da04ef8517dcf960873ebb98e2ce777d55dae21
Grant Paulson <grant.paulson@appfolio.com>
2019-08-14 11:01:32 -0700

    style and update content for landing page

--- test/controllers/images_controller_test.rb
+++ test/controllers/images_controller_test.rb
@@ -1,9 +1,6 @@
 require 'test_helper'
 
 class ImagesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
 
   test "should get home" do
     get '/'

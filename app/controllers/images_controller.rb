879d9531b3be1f127d921fd4140d4da4c0d54992
Grant Paulson <grant.paulson@appfolio.com>
2019-08-15 15:55:14 -0700

    gp - add controller actions for creating and showing image

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -4,10 +4,25 @@ class ImagesController < ApplicationController
   end
 
   def new
+    @image = Image.new
     render
   end
 
   def create
+    @image = Image.new(image_params)
+    if @image.save
+      redirect_to :show
+    end
+  end
 
+  private
+
+  def image_params
+    debugger
+    params.require(:image).permit(:name, :url)
+  end
+
+  def show
+    render
   end
 end

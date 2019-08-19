e103d66c5c415ee3cefa63e7caa630da2262db8c
Grant Paulson <grant.paulson@appfolio.com>
2019-08-19 12:40:49 -0700

    gp - fix bug with fixture stub causing test errors

--- db/migrate/20190816221001_create_images.rb
+++ db/migrate/20190816221001_create_images.rb
@@ -3,6 +3,7 @@ class CreateImages < ActiveRecord::Migration[5.2]
     create_table :images do |t|
       t.string :name, null: false
       t.string :url, null: false
+
       t.timestamps
     end
   end

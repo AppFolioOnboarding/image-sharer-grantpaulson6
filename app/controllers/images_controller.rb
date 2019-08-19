91cdc771f5914368b162d92e9b3b12743446aecc
Grant Paulson <grant.paulson@appfolio.com>
2019-08-19 12:45:24 -0700

    gp - add url validations and error display

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -11,13 +11,18 @@ class ImagesController < ApplicationController
   def create
     @image = Image.new(image_params)
     if @image.save
-      render :show
+      # flash[:notice] = 'Image url successfully saved.'
+      redirect_to image_path @image, notice: 'Image url successfully saved.'
     else
-
+      flash[:errors] = @image.errors.messages
+      render :new, status: :unprocessable_entity
+      # QUESTION: would we want to redirect_to new_image_path to maintain consistent URL for client? (shows as
+      # /images after failure)
     end
   end
 
   def show
+    @image = Image.find(params[:id])
     render
   end
 
--- app/models/image.rb
+++ app/models/image.rb
@@ -1,3 +1,18 @@
+require 'uri'
+
 class Image < ApplicationRecord
   validates :name, :url, presence: true
+  validate :valid_url?
+
+  # should this be in a validator class?
+  def valid_url?
+    # is this a proper way of handling errors? of using control flow?
+    if URI.parse(url).host.nil?
+      errors.messages[:url] ||= []
+      errors.messages[:url].push('must be valid.')
+    end
+  rescue URI::InvalidURIError
+    errors.messages[:url] ||= []
+    errors.messages[:url].push('must be valid.')
+  end
 end

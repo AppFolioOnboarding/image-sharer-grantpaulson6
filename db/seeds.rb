851a4bac5b71e2b36c3c920094fcf99a4f449907
Grant Paulson <grant.paulson@appfolio.com>
2019-08-20 16:32:35 -0700

    gp - Add seed data using NPS API

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require 'http'
+require 'json'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +7,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Image.destroy_all
+
+body = HTTP.get('https://developer.nps.gov/api/v1/parks?fields=fullName,images,description,states&limit=100&api_key=FgGdhhL5SPzrAxDjtp8fJhqeG8KgUWSfHet86gzJ').parse
+
+# data = JSON.parse(body)
+
+body['data'].each do |park|
+  if park['fullName'] =~ /Park|Preserve/ && park['images'] && !park['images'].empty?
+    random_pick = rand(park['images'].length)
+    Image.create!(name: park['fullName'], url: park['images'][random_pick]['url'])
+  end
+end

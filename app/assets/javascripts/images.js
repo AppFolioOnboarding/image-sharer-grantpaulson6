e5894393684b32908393ab410baa8b0d5d9ecc0c
Grant Paulson <grant.paulson@appfolio.com>
2019-08-22 10:52:26 -0700

    gp - Add styling and bootstrap

--- app/assets/javascripts/images.js
+++ app/assets/javascripts/images.js
@@ -1,2 +1,7 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+// document.querySelector('.nav .nav-link').on('click', () => {
+//   document.querySelector('.nav').find('.active').removeClass('active');
+//   this.addClass('active');
+// });
